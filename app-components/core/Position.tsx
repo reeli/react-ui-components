d4113129b093c053f2afd5bda2c04924a46596c4
HelloRuiLi <1229668446@qq.com>
2018-04-02 22:39:03 +0800

    update position when resize and mouse wheel

--- app-components/core/Position.tsx
+++ app-components/core/Position.tsx
@@ -7,14 +7,32 @@ interface IPositionProps {
 }
 
 export class Position extends Component<IPositionProps, any> {
+  private ele: Element | null = null;
   state = {
     position: null,
   };
 
   componentDidMount() {
+    this.ele = findDOMNode(this);
     this.setState({
-      position: findDOMNode(this).getBoundingClientRect(),
+      position: this.ele.getBoundingClientRect(),
+    }, () => {
+      document.body.addEventListener('wheel', this.updatePosition);
+      document.body.addEventListener('resize', this.updatePosition);
     });
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('wheel', this.updatePosition);
+    document.body.removeEventListener('resize', this.updatePosition);
+  }
+
+  updatePosition = () => {
+    if (this.ele) {
+      this.setState({
+        position: this.ele.getBoundingClientRect(),
+      });
+    }
   }
 
   render() {
