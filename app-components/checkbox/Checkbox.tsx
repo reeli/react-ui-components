fd3c64a06683bdabce9f247aea868f638cf9fff1
HelloRuiLi <1229668446@qq.com>
2018-04-05 17:12:54 +0800

    pretty checkbox

--- app-components/checkbox/Checkbox.tsx
+++ app-components/checkbox/Checkbox.tsx
@@ -1,3 +1,4 @@
+import { css } from 'glamor';
 import { isEqual } from 'lodash';
 import * as React from 'react';
 
@@ -6,6 +7,16 @@ interface ICheckboxProps {
   onChange: (value: boolean) => void;
   label?: string | number;
 }
+
+const inputWrapper = css({
+  position: 'relative',
+});
+
+const inputStyles = css({
+  width: 0,
+  height: 0,
+  visibility: 'hidden',
+});
 
 export class Checkbox extends React.Component<ICheckboxProps, any> {
   state = {
@@ -31,11 +42,26 @@ export class Checkbox extends React.Component<ICheckboxProps, any> {
   render() {
     return <label>
       {this.props.label && <span>{this.props.label}</span>}
-      <input
-        type='checkbox'
-        checked={this.state.value}
-        onChange={this.handleChange}
-      />
+      <div {...inputWrapper}>
+        {this.state.value
+          ? <svg fill="#00a862" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+          </svg>
+          : <svg xmlSpace="preserve" x="0" y="0" fill="#00a862" height="24" viewBox="0 0 24 24" width="24"
+                 xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
+        }
+        <input
+          type='checkbox'
+          checked={this.state.value}
+          onChange={this.handleChange}
+          {...inputStyles}
+        />
+      </div>
     </label>
   }
 }
