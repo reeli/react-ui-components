d9e1c946b583cf3fd0426b277cf660bb64a4a59e
HelloRuiLi <rrli@thoughtworks.com>
2018-05-01 15:55:35 +0800

    extract donut progress component

---
+++ app-components/donut-progress/__examples__/DonutProgressDemo.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { DonutProgress } from "../DonutProgress";
+
+export class DonutProgressDemo extends React.Component<any, any> {
+  render() {
+    return <DonutProgress />;
+  }
+}
