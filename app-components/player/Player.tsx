06d83e55e01e6c71be1f345c0e1c4eae49b68c52
HelloRuiLi <rrli@thoughtworks.com>
2018-08-17 10:06:30 +0800

    init player

---
+++ app-components/player/Player.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+// const audioLink =
+//   'http://m10.music.126.net/20180717155731/79b976cb23188684b0f8e1fc5416aa8c/ymusic/4cc1/66e6/f67b/a3c078f42299698716d6eea4ca5c4057.mp3';
+
+export class Player extends React.Component {
+  render() {
+    return (
+      <div>
+        <text>test</text>
+      </div>
+    );
+  }
+}
---
+++ app-components/player/__examples__/PlayerDemo.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Player } from "../Player";
+
+export class PlayerDemo extends React.Component<any, any> {
+  render() {
+    return (
+      <Player />
+    );
+  }
+}
