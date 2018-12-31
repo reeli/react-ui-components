09d9ec49f2a7b3554917556fe4cc80d2a634b490
reeli <reeli.null@gmail.com>
2018-12-31 15:39:11 +0800

    fix: fix ts compile issue

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,13 @@
-import * as HtmlWebpackPlugin from 'html-webpack-plugin';
-import * as path from 'path';
-import * as webpack from 'webpack';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import * as path from "path";
+import * as webpack from "webpack";
 
 const webpackConfig: webpack.Configuration = {
-  context: path.resolve(__dirname, './app-style-guide'),
-  entry: './index.tsx',
+  context: path.resolve(__dirname, "./app-style-guide"),
+  entry: "./index.tsx",
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, "public"),
+    filename: "[name]-[hash].js",
   },
   module: {
     rules: [
@@ -15,12 +15,12 @@ const webpackConfig: webpack.Configuration = {
         test: /(\.ts|\.tsx)$/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
               transpileOnly: true,
               compilerOptions: {
-                module: 'es6',
-                target: 'es5',
+                module: "es6",
+                target: "es5",
               },
             },
           },
@@ -28,17 +28,17 @@ const webpackConfig: webpack.Configuration = {
       },
       {
         test: /\.html$/,
-        use: ['raw-loader'],
+        use: ["raw-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'React UI Components',
-      template: './index.html',
+      title: "React UI Components",
+      template: "./index.html",
     }),
   ],
-  mode: 'development',
+  mode: "development",
 };
 
 export = webpackConfig;
