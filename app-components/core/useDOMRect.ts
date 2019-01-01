a89a5b2ba91581dab37797d68d607b7b3940ed16
reeli <reeli.null@gmail.com>
2019-01-01 17:46:12 +0800

    feat: create useDOMRect hook

---
+++ app-components/core/useDOMRect.ts
@@ -0,0 +1,25 @@
+import { RefObject, useLayoutEffect, useState } from "react";
+
+export const useDOMRect = (ele: RefObject<HTMLElement | null>, inputs: ReadonlyArray<any> = []): ClientRect | null => {
+  const [domRect, updateDOMRect] = useState<ClientRect | null>(null);
+
+  useLayoutEffect(() => {
+    const getRect = () => {
+      if (ele.current) {
+        updateDOMRect(ele.current!.getBoundingClientRect());
+      }
+    };
+
+    getRect();
+
+    document.body.addEventListener("wheel", getRect);
+    window.addEventListener("resize", getRect);
+
+    return function cleanup() {
+      document.body.removeEventListener("wheel", getRect);
+      window.removeEventListener("resize", getRect);
+    };
+  }, inputs);
+
+  return domRect;
+};
