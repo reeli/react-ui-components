22c84ab8a91eae9bdbe64738a68c73769ea72514
reeli <reeli.null@gmail.com>
2019-01-09 22:43:21 +0800

    refactor: refactor useToggle

--- app-components/portal/useToggle.tsx
+++ app-components/portal/useToggle.tsx
@@ -1,22 +1,19 @@
 import { useMemo, useState } from "react";
 
-export const useToggle = (defaultState: boolean = false) => {
-  const [isShow, setShowState] = useState(defaultState);
+export const useToggle = (defaultVisible: boolean = false) => {
+  const [visible, setVisible] = useState(defaultVisible);
 
   /*
-       `useMemo` here to ensure `open` and `close` only be created once.
-        Otherwise, it would be create again every time when the Function component re-render.
-        */
+         `useMemo` here to ensure `open` and `close` only be created once.
+          Otherwise, it would be create again every time when the Function component re-render.
+          */
 
   const { show, hide } = useMemo(() => {
-    const create = (state: boolean) => () => {
-      setShowState(state);
-    };
     return {
-      show: create(true),
-      hide: create(false),
+      show: () => setVisible(true),
+      hide: () => setVisible(false),
     };
   }, []);
 
-  return [isShow, show, hide] as [typeof isShow, typeof show, typeof hide];
+  return [visible, show, hide] as [typeof visible, typeof show, typeof hide];
 };
