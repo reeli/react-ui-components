af08a06171f05633f5bbae56880d2cf8bb14b370
reeli <reeli.null@gmail.com>
2020-02-20 13:25:57 +0800

    feat: add hook

---
+++ src/core/must.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { isEmpty, some } from "lodash";
+
+export function must<TPrepares extends Readonly<Array<any>>>(usePrepares: (props: unknown) => TPrepares) {
+  return function<TProps>(render: (props: TProps, ...prepares: TPrepares) => JSX.Element | null) {
+    const C = ({ "data-prepares": dataPrepares, ...props }: { "data-prepares": TPrepares } & TProps) =>
+      render(props as any, ...dataPrepares);
+
+    return function Must(props: TProps) {
+      const prepares = usePrepares(props);
+
+      if (some(prepares, isEmpty)) {
+        return null;
+      }
+
+      return <C {...props} data-prepares={prepares} />;
+    };
+  };
+}
