eb17d15393f562af63d2e79556f1c391e3fdd49e
reeli <reeli.null@gmail.com>
2020-02-24 17:23:41 +0800

    feat: remove useless style in ModalOverlay.tsx

--- src/modal/ModalOverlay.tsx
+++ src/modal/ModalOverlay.tsx
@@ -14,7 +14,6 @@ export const ModalOverlay = ({ onClick }: IModalOverlayProps) => (
       bottom: 0,
       right: 0,
       background: "rgba(0,0,0,0.65)",
-      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     }}
   />
 );
