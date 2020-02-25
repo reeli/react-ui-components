1208964caf5b76929e4a26744ea8e2d93f354876
reeli <reeli.null@gmail.com>
2020-02-25 17:00:08 +0800

    refactor: use Portal component refactor usePortal.tsx

--- src/portal/usePortal.tsx
+++ src/portal/usePortal.tsx
@@ -1,39 +1,20 @@
-import { createPortal } from "react-dom";
-import React, { ReactNode, useContext, useEffect, useMemo, useRef } from "react";
-import { PortalContext, PortalProvider } from "src/portal/PortalContext";
-import { useRefValue, useToggle } from "src/core";
+import React, { ReactNode, useMemo, useRef } from "react";
+import { useToggle } from "src/core";
+import { Portal } from "src/portal/Portal";
 
 export const usePortal = (defaultVisible = false) => {
   const [visible, show, hide] = useToggle(defaultVisible);
-  const visibleRef = useRefValue(visible);
-  const { container: rootContainer } = useContext(PortalContext);
-  const containerRef = useRef<HTMLElement | null>(null);
 
-  if (!containerRef.current) {
-    containerRef.current = document.createElement("div");
-    containerRef.current.setAttribute("role", "portal");
-    rootContainer.appendChild(containerRef.current);
-  }
+  // const visibleRef = useRefValue(visible);
+  const visibleRef = useRef(visible);
+  visibleRef.current = visible;
 
-  useEffect(() => {
-    return function cleanup() {
-      if (containerRef.current) {
-        rootContainer.removeChild(containerRef.current);
-      }
-    };
-  }, []);
-
+  // renderPortal 会在组件 render 时被调用
   const renderPortal = useMemo(
-    () => (children: ReactNode) =>
-      visibleRef.current && (
-        <PortalProvider
-          value={{
-            container: containerRef.current!,
-          }}
-        >
-          {createPortal(children, containerRef.current!)}
-        </PortalProvider>
-      ),
+    () => (children: ReactNode) => {
+      // 由于此时取值是 在 render 时，如果使用 useRefValue，那么这里取到的 visible 的值就始终是前一次的值（因为要等 render 完成之后才会给 visibleRef.current 赋值），而非最新的值。也就是说取值先于赋值。
+      return visibleRef.current && <Portal>{children}</Portal>;
+    },
     [],
   );
 
