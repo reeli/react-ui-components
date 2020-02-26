0b6a3d9a0f7af6d9720a0be24736846cafbbfbb6
reeli <reeli.null@gmail.com>
2020-02-26 22:06:04 +0800

    feat: add Placement.over in getPlacement.ts

--- src/core/utils/getPlacement.ts
+++ src/core/utils/getPlacement.ts
@@ -11,6 +11,7 @@ export enum Placement {
   bottom,
   bottomLeft,
   bottomRight,
+  over,
 }
 
 interface IPosition {
@@ -81,9 +82,13 @@ const getPosition = (triggerRect: ClientRect | null, contentRect: ClientRect | n
       case Placement.rightBottom:
         position.left += triggerRect.left + triggerRect.width;
         position.top += triggerRect.top + dHeight;
-        // case Placement.rightAuto:  把 trigger.left 和 clientWidth 作比较，如果大于 clientWith/2，证明它在右边，应该显示在左面
-        //   position.left += triggerRect.left + triggerRect.width;
-        //   position.top += triggerRect.top + dHeight;
+        break;
+      // case Placement.rightAuto:  把 trigger.left 和 clientWidth 作比较，如果大于 clientWith/2，证明它在右边，应该显示在左面
+      //   position.left += triggerRect.left + triggerRect.width;
+      //   position.top += triggerRect.top + dHeight;
+      case Placement.over:
+        position.left = triggerRect.left;
+        position.top = triggerRect.top;
         break;
     }
   }
