5a42f189a98532f89ece5e9a99d5760ddd57dfdd
reeli <reeli.null@gmail.com>
2020-02-27 21:31:35 +0800

    feat: add invariant check for Popover.tsx

--- src/popover/Popover.tsx
+++ src/popover/Popover.tsx
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { IOverlayTriggerProps } from "src/core/components/OverlayTrigger";
 import { usePortal } from "src/portal/usePortal";
 import { Position } from "src/core/components/Position";
 import { ClickAwayListener } from "src/core/components/ClickAwayListener";
+import invariant from "invariant";
 
 interface IPopoverProps extends IOverlayTriggerProps {
   defaultVisible?: boolean;
@@ -11,6 +12,13 @@ interface IPopoverProps extends IOverlayTriggerProps {
 export const Popover: React.FC<IPopoverProps> = ({ content, children, placement, defaultVisible = false }) => {
   const triggerEl = useRef<HTMLElement>(null);
   const [renderPortal, show, hide] = usePortal(defaultVisible);
+
+  useEffect(() => {
+    invariant(
+      triggerEl.current instanceof HTMLElement,
+      "The children must be able to receive ref prop of HTMLElement.",
+    );
+  }, []);
 
   return (
     <>
--- src/popover/usePopover.tsx
+++ src/popover/usePopover.tsx
@@ -1,12 +1,20 @@
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { usePortal } from "src/portal/usePortal";
 import { Position } from "src/core/components/Position";
 import { Placement } from "src/core";
 import { ClickAwayListener } from "src/core/components/ClickAwayListener";
+import invariant from "invariant";
 
 export const usePopover = (defaultVisible = false) => {
   const [renderPortal, show, hide, visible] = usePortal(defaultVisible);
   const triggerEl = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    invariant(
+      triggerEl.current instanceof HTMLElement,
+      "The children must be able to receive ref prop of HTMLElement.",
+    );
+  }, []);
 
   const renderPopoverContent = useMemo(() => {
     return (content: React.ReactNode, placement: Placement = Placement.bottomLeft) =>
