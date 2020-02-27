199c3ca431ee906b8ed36e44992cb6f5efdbfe92
reeli <reeli.null@gmail.com>
2020-02-27 22:21:15 +0800

    feat: create useTooltip.tsx

---
+++ src/tooltip/useTooltip.tsx
@@ -0,0 +1,54 @@
+import React, { PropsWithChildren, useEffect, useMemo, useRef } from "react";
+import invariant from "invariant";
+import { Position } from "src/core/components/Position";
+import { Placement, useToggle } from "src/core";
+import { Portal } from "src/portal";
+
+export const useTooltip = () => {
+  const triggerEl = useRef<HTMLElement>(null);
+  const [visible, show, hide] = useToggle();
+
+  const visibleRef = useRef(visible);
+  visibleRef.current = visible;
+
+  useEffect(() => {
+    invariant(
+      triggerEl.current instanceof HTMLElement,
+      "The children must be able to receive ref prop of HTMLElement.",
+    );
+  }, []);
+
+  const TooltipContent = useMemo(() => {
+    return ({ children, placement }: PropsWithChildren<{ placement?: Placement }>) => {
+      return (
+        <Portal>
+          <Position triggerRef={triggerEl} placement={placement}>
+            {children}
+          </Position>
+        </Portal>
+      );
+    };
+  }, []);
+
+  const TooltipTrigger = useMemo(() => {
+    return ({ children }: { children: React.ReactElement }) => {
+      return React.cloneElement(React.Children.only(children), {
+        ref: triggerEl,
+        onMouseEnter: () => {
+          console.log("enter");
+          show();
+        },
+        onMouseLeave: hide,
+        onMouseOver: () => {
+          if (visibleRef.current) {
+            return;
+          }
+          console.log("hover");
+          show();
+        },
+      });
+    };
+  }, []);
+
+  return [TooltipTrigger, TooltipContent, show, hide, visible] as const;
+};
