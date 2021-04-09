00c6047a695a410f56fc6061db3d00f85101f88b
reeli <reeli.null@gmail.com>
2021-04-10 00:34:58 +0800

    add picker

--- src/picker/Picker1.tsx
+++ src/picker/Picker1.tsx
@@ -1,6 +1,4 @@
-import React, { useRef } from "react";
-import { animated, useSpring } from "react-spring";
-import { useGesture } from "react-use-gesture";
+import React, { useRef, useState } from "react";
 
 interface Option {
   label: string;
@@ -20,58 +18,8 @@ interface PickerProps {
 export const Picker: React.FC<PickerProps> = ({ options, onChange }) => {
   const itemHeight = 40;
   const containerHeight = 400;
-  const offsetItemCount = 2;
-  const [{ y }, set] = useSpring(() => ({ y: 0 }));
-  const offsetYRef = useRef(0);
-
-  const bind = useGesture({
-    onDragStart: () => {
-      offsetYRef.current = y.get();
-    },
-    onDrag: ({ movement }) => {
-      const nextY = offsetYRef.current + movement[1];
-
-      if (nextY > itemHeight * offsetItemCount) {
-        return;
-      }
-      if (nextY < -itemHeight * (options.length - 1 + offsetItemCount)) {
-        return;
-      }
-
-      return set({ y: nextY });
-    },
-    onDragEnd: ({ movement, elapsedTime }) => {
-      let nextY = offsetYRef.current + movement[1];
-
-      if (elapsedTime < 200) {
-        console.log(movement[1] / elapsedTime, "v");
-
-        // 惯性 v = s/t
-        // https://github.com/Tencent/weui.js/blob/master/src/picker/scroll.js#L176
-        // 要实现惯性效果，需要计算出到达指定 item 之后额外运行的距离 d, d = v* 150(magic number)
-        nextY = nextY + (movement[1] / elapsedTime) * 150;
-      }
-
-      if (nextY >= 0) {
-        set({ y: 0 });
-        onChange(getValueByIdx(0));
-
-        return;
-      }
-      if (nextY <= -(options.length - 1) * itemHeight) {
-        const idx = options.length - 1;
-        set({ y: -(options.length - 1) * itemHeight });
-
-        onChange(getValueByIdx(idx));
-        return;
-      }
-
-      const index = Math.round(nextY / itemHeight);
-      set({ y: index * itemHeight });
-
-      onChange(getValueByIdx(Math.abs(index)));
-    },
-  });
+  const offsetYStartRef = useRef<number>(0);
+  const [deltaY, setDeltaY] = useState(0);
 
   // const [index, setIndex] = useState(0);
 
@@ -87,7 +35,6 @@ export const Picker: React.FC<PickerProps> = ({ options, onChange }) => {
         height: containerHeight,
         border: "1px solid blue",
         overflow: "hidden",
-        touchAction: "none",
       }}
     >
       <div
@@ -100,7 +47,48 @@ export const Picker: React.FC<PickerProps> = ({ options, onChange }) => {
           zIndex: 3,
           backgroundColor: "rgba(0,0,0,0.3)",
         }}
-        {...bind()}
+        onTouchStart={(e) => {
+          offsetYStartRef.current = Math.floor(e.touches[0].clientY);
+        }}
+        onTouchMove={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          // console.log(e.touches[0],'e.touches[0]')
+          const currentY = Math.floor(e.touches[0].clientY);
+          setDeltaY((prevDeltaY) => {
+            const dY = prevDeltaY + (currentY - offsetYStartRef.current);
+
+            if (dY > itemHeight || dY < -options.length * itemHeight) {
+              return prevDeltaY;
+            }
+
+            // 1. setState deltaY 时需要更新 offsetYStartRef
+            offsetYStartRef.current = currentY;
+
+            return dY;
+          });
+        }}
+        onTouchEnd={() => {
+          if (deltaY >= 0) {
+            setDeltaY(0);
+            // setIndex(0);
+            onChange(getValueByIdx(0));
+
+            return;
+          }
+          if (deltaY <= -(options.length - 1) * itemHeight) {
+            setDeltaY(-(options.length - 1) * itemHeight);
+            const idx = options.length - 1;
+            // setIndex(options.length - 1);
+            onChange(getValueByIdx(idx));
+            return;
+          }
+
+          const index = Math.round(deltaY / itemHeight);
+          setDeltaY(index * itemHeight);
+          // setIndex(Math.abs(index));
+          onChange(getValueByIdx(Math.abs(index)));
+        }}
       />
       <div
         className={"indicator"}
@@ -114,7 +102,7 @@ export const Picker: React.FC<PickerProps> = ({ options, onChange }) => {
           zIndex: 2,
         }}
       />
-      <animated.div
+      <div
         className={"content"}
         css={{
           width: "100%",
@@ -124,16 +112,16 @@ export const Picker: React.FC<PickerProps> = ({ options, onChange }) => {
           zIndex: 1,
           backgroundColor: "pink",
         }}
-        style={{ y }}
+        style={{ transform: `translateY(${deltaY}px)` }}
       >
         {options.map((option, index) => {
           return (
-            <div css={{ height: itemHeight, lineHeight: `${itemHeight}px` }} key={index}>
+            <div css={{ height: itemHeight }} key={index}>
               {option.label}
             </div>
           );
         })}
-      </animated.div>
+      </div>
     </div>
   );
 };
