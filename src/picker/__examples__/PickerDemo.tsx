8b5767767e608f05851bded4815a7f8439fd5ab5
reeli <reeli.null@gamil.com>
2021-04-10 18:12:41 +0800

    test

--- src/picker/__examples__/PickerDemo.tsx
+++ src/picker/__examples__/PickerDemo.tsx
@@ -58,6 +58,7 @@ export class PickerDemo2 extends React.Component<any, any> {
         value={this.state.value}
         onChange={(value) => {
           alert(value);
+          console.log(value, "selected date");
           this.setState({
             value,
           });
