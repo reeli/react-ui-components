03f2fa02e75fdbaaee632d015cbe02e6ef1528cc
reeli <reeli.null@gmail.com>
2023-05-31 18:57:34 +0800

    update button demo

--- packages/ui-core/src/button/__examples__/ButtonDemo.tsx
+++ packages/ui-core/src/button/__examples__/ButtonDemo.tsx
@@ -1,12 +1,17 @@
 import { Button } from "../Button";
+import { Box } from "@ui/base";
 
 export function ButtonDemo() {
   return (
-    <div>
+    <Box component={"div"} sx={{ display: "flex", gap: 10 }}>
       <Button onClick={() => console.log("hello")}>button1</Button>
       <Button onClick={() => console.log("good")} disabled>
         button2
       </Button>
-    </div>
+      <Button onClick={() => console.log("good")} disabled>
+        Button3
+        <div>icon</div>
+      </Button>
+    </Box>
   );
 }
